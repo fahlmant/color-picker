574f67c1b96eb4874beb16b697c2bcd03e665569
fahlmant <taylor@bridgetowngamestudios.com>
2024-06-30 12:56:49 -0700

    Remove conversions to BMP and to imgConfig as they're redundant

--- main.go
+++ main.go
@@ -11,31 +11,34 @@ import (
 	"net/http"
 	"os"
 	"sort"
-
-	"golang.org/x/image/bmp"
 )
 
 func main() {
 
+	// Flag for specifying how many results to return. For example, --results 5 shows the top 5 hex values by pixel count
 	numResults := flag.Int("results", 10, "Specify the number of results to return")
 	flag.Parse()
 
+	// Get file path
 	fileName := flag.Args()
 	if len(fileName) > 1 || len(fileName) == 0 {
 		fmt.Println("Expected one file, exiting")
 		return
 	}
 
+	// Read the file contents into a byte array
 	fileBytes, err := os.ReadFile(fileName[0])
 	if err != nil {
 		// replace this with real error handling
 		panic(err.Error())
 	}
 
+	// Attempt to extract the content type of the file, looking for an image format
 	contentType := http.DetectContentType(fileBytes)
 
 	var img goimage.Image
 
+	// Decode PNGs, JPEGs and GIFs into Go Image type
 	switch contentType {
 	case "image/png":
 		img, err = png.Decode(bytes.NewReader(fileBytes))
@@ -58,28 +61,13 @@ func main() {
 		}
 	}
 
-	buf := new(bytes.Buffer)
-	if err := bmp.Encode(buf, img); err != nil {
-		fmt.Printf("Error: %+v\n", err)
-		return
-	}
-
-	bmpImage, err := bmp.Decode(buf)
-	if err != nil {
-		fmt.Printf("Error: %+v\n", err)
-		return
-	}
-
-	imgConfig, _, err := goimage.Decode(bytes.NewReader(fileBytes))
-	if err != nil {
-		fmt.Printf("Img Config Error: %+v\n", err)
-		return
-	}
-
+	// Create a map for tracking hexcode frequencies
 	hexCodeFreq := make(map[string]int)
-	for i := 0; i < imgConfig.Bounds().Dx(); i++ {
-		for j := 0; j < imgConfig.Bounds().Dy(); j++ {
-			r, g, b, _ := bmpImage.At(i, j).RGBA()
+
+	// Find the hexcode for each pixel and increment the frequency in the map for that hex code
+	for i := 0; i < img.Bounds().Dx(); i++ {
+		for j := 0; j < img.Bounds().Dy(); j++ {
+			r, g, b, _ := img.At(i, j).RGBA()
 			// RGBA returns a number between 0 and 65535. 255 * 257 = 65535, so we divide by 257 to get a number between 0 and 255
 			hexCode := fmt.Sprintf("#%02x%02x%02x\n", uint32((float64(r) / 257)), uint32((float64(g) / 257)), uint32((float64(b) / 257)))
 			// Increases the counter for a hex string for each occurence
